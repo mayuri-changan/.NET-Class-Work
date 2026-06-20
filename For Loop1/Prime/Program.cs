00fc80112c50d9b1261375f5189223d30ee7c222
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-20 19:48:38 +0530

    Loop1

---
+++ For Loop1/Prime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           for(int i=1;i<=10;i++)
+            {
+              
+                System.Console.WriteLine(i*2);
+               
+            }
+
+        }
+    }
+}
---
+++ For Loop1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int i=0;i<=5;i++)
+            {
+                for(int  j=0;j<=5;j++)
+                {
+                   
+                    System.Console.WriteLine(i+j);
+                   
+
+
+                }
+            }
+            
+        }
+        }
+    }
+
