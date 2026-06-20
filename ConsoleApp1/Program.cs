ba32b89bd38ed771dbc4ee56822029f2d7115d5a
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-20 19:47:12 +0530

    consoleApp1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            System.Console.WriteLine("Hello World");
+            System.Console.WriteLine("Hii Mayuri");
+        }
+    }
+}
