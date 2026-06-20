471f6987ebfb5dc5a904ea9192e89e9627830386
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-20 19:49:23 +0530

    Loop2

---
+++ ForLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<=5;i++)
+            {
+
+                System.Console.WriteLine(i);
+            }
+        }
+    }
+}
