6123b79ce99a6906b68b26fdefd903d01e70f857
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-21 20:27:54 +0530

    Demo02

---
+++ Demo02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Demo02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=================Star Printing=====================");
+            for(int i=0; i <=2; i++)
+            {
+                for(int j=1; j <=5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
