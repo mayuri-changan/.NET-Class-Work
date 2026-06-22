c2bc4950c45ae8d93a052edc399b6d594cf069cf
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-22 21:49:27 +0530

    Demo05

---
+++ Demo05/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Demo05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDemo obj = new MyClass();
+            obj.Foo();
+            //obj.Log();
+        }
+    }
+
+    public interface IDemo
+    {
+        public static int id;
+        void Foo();
+        protected void Log()
+        {
+            Console.WriteLine("Log method from IDemo interface");
+        }
+    }
+
+    public class MyClass : IDemo
+    {
+        public void Foo()
+        {
+            Console.WriteLine("Foo method from MyClass");
+        }
+        protected void Log()
+        {
+            Console.WriteLine("Log method from MyClass");
+        }
+    }
+}
+
