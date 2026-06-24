690941d5c7326c4ea225c098fd72d8031a1534f9
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-24 22:50:23 +0530

    Demo06

---
+++ Demo06/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo06
+{
+    public class Employee
+    {
+        public string Department;
+         
+        public Employee()
+        {
+
+        }
+        public Employee(string department)
+        {
+            Department = department;
+
+        }
+
+    }
+    public class Developers : Employee
+    {
+        public string Projects;
+        public Developers() : base()
+        {
+        }
+        public Developers(string projectName, string dpt) : base(dpt)
+        {
+            Projects = projectName;
+        }
+
+
+    }
+}
+
---
+++ Demo06/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Demo06")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Demo06")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1ef2666b-6494-4944-bf86-6560ece06417")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
