bb08fc1013c58441b8b1bf5079b15ffe28162274
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-26 22:39:50 +0530

    Demo12

---
+++ Demo12/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+namespace Demo12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo demo = new Demo();
+            demo.WrapperMethod();
+
+            HR hr = new HR();
+            hr.Display();
+        }
+    }
+
+    public class Person
+    {
+        public virtual string Name { get; set; }
+        public virtual void Display()
+        {
+            Console.WriteLine("Person : Display");
+        }
+    }
+    public class Employee : Person
+    {
+        public sealed override string Name { get => base.Name; set => base.Name = value; }
+        public sealed override void Display()
+        {
+            Console.WriteLine("Employee: Display");
+        }
+    }
+    public sealed class HR : Employee
+    {
+        //public override string Name { get => base.Name; set => base.Name = value; }
+        //public override void Display()
+        //{
+        //    Console.WriteLine("HR: Display");
+
+        //}
+        public void EmpData()
+        {
+           
+            Console.WriteLine("EmpData: HR");
+        }
+    }
+    public class Demo 
+    {
+        public void WrapperMethod()
+        {
+            HR obj = new HR();
+            obj.EmpData();
+        }
+    }
+
+}
+
