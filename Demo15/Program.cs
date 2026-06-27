5d92a1d91ec8abe543b038226e8cb4f5b5e73cc0
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-27 22:07:07 +0530

    Demo15

---
+++ Demo15/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Demo14;
+namespace Demo15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            CMath cmath=new CMath();
+            cmath.Add(10, 20);
+            Test test =new Test();
+            test.TestWrapperMethod();
+
+        }
+        public class Test : CMath 
+        {
+            public void TestWrapperMethod()
+            {
+                Add(4, 4);
+                mul(4, 4);
+                Squ(3);
+
+            }
+        
+        }
+
+    }
+}
