28cf446ccbcdf718607ddb7c13936743b52ad31d
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-29 23:12:01 +0530

    Demo16.1

---
+++ Demo16.1/Logics/Emp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo16._1.Logics
+{
+    
+    internal class Emp
+    {
+        private string _FullName;
+        public string FullName
+        {
+            get { return _FullName; }
+            set { _FullName = value; }
+        }
+    }
+}
+
---
+++ Demo16.1/Program.cs
@@ -0,0 +1,11 @@
+﻿using Demo16._1;
+using Demo16._1.Logics;
+
+// UI : Buissness Presentation Logic
+Demo demo = new Demo();
+
+demo.DemoMethod();
+
+Emp emp = new Emp();
+emp.FullName = "Hugh Jackman";
+Console.WriteLine(emp.FullName);
