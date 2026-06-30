fe46142470b0d22708f6ddc4cfdf8d8da06b1793
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-30 23:05:29 +0530

    Demo22

---
+++ Demo22/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Demo22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------Collection------");
+            Book book=new Book();
+            book.BookName = "Let us c";
+            book.AuthorName = "Yashwant";
+            Console.WriteLine(book.BookName);
+            Console.WriteLine(book.AuthorName);
+
+            
+
+        }
+        public class Book
+        {
+            private string _BookName;
+            private string _AuthorName;
+            public string AuthorName
+            {
+                get
+                {
+                    return _AuthorName;
+
+                }
+                set
+                {
+                    _AuthorName = value;
+                }
+            }
+                public string BookName
+                {
+                get
+                {
+                    return _BookName;
+                }
+                set
+                {
+                    _BookName = value;
+
+                }
+
+                }
+
+            }
+
+
+        }
+    }
+
