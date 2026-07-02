22df0291fda9638eed8eb7d03c79a86700ca932c
mayuri-changan <mayuri2001changan@gmail.com>
2026-07-02 15:40:15 +0530

    Demo2

---
+++ Demo24/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Demo24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n");
+        }
+        public class MyClass
+        {
+        }
+        public class CMath
+        {
+
+        }
+
+
+
+    }
+}
