818e7a70de24538eb6ae739886bdc30cb4dad90b
mayuri-changan <mayuri2001changan@gmail.com>
2026-07-03 21:56:02 +0530

    Demo27

---
+++ Demo27/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Demo27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("=======================================EXCEPTION HANDLING======================================================\n");
+            int x = 100;
+            int y = 2;
+            try
+            {
+                int div = x / y;
+                Console.WriteLine($"Div result:{div}");
+
+            }
+            catch( Exception ex ) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Every time finally block gets called !!!!!");
+            }
+        }
+        public class MyCustomException : Exception 
+        { 
+            public MyCustomException(string message): base(message)
+            {
+            }
+        
+        }
+
+
+
+
+    }
+}
