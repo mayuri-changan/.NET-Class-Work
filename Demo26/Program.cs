a55843c8f6f5d559b6015d3bcff6e4c6eee59dcb
mayuri-changan <mayuri2001changan@gmail.com>
2026-07-03 21:55:09 +0530

    Demo2

---
+++ Demo26/Program.cs
@@ -0,0 +1,32 @@
+﻿//using statement:namespace resolve
+using System;
+
+namespace Demo26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".......................................GARBAGE COLLECTOR.............................................\n");
+            MyClass obj=new MyClass();
+            MyClass obj2= new MyClass();
+            
+            obj.Dispose();
+            obj2.Show();
+        }
+        public class MyClass: IDisposable
+        {
+            public void Dispose()
+            {
+                
+                Console.WriteLine("Disposal method gets called!!!!!!");
+                GC.SuppressFinalize(this);
+            }
+            public void Show()
+            {
+                Console.WriteLine("show method in garbage collector");
+            }
+
+        }
+    }
+}
