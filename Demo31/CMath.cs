a9553b0ca1722ef6d7488d05247e89056c67ef4a
mayuri-changan <mayuri2001changan@gmail.com>
2026-07-05 10:18:43 +0530

    Demo31

---
+++ Demo31/CMath.cs
@@ -0,0 +1,16 @@
+﻿namespace Demo31
+{
+    public class CMath
+    {
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        
+        }
+
+    }
+}
