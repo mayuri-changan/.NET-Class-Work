1351977f0337cda69748e051af0ff121317ee395
mayuri-changan <mayuri2001changan@gmail.com>
2026-07-06 11:40:42 +0530

    Demo34.1

---
+++ Demo34.1/Emp.cs
@@ -0,0 +1,38 @@
+﻿using Demo33;
+using System.Text.Json.Serialization;
+using Demo36;
+
+namespace Demo34._1
+{
+    [Table(TableName = "Emp")]
+    public class Emp
+    {
+        
+        
+            private int _EId;
+            private string _EName;
+            private string _EAddress;
+
+        [Column(ColumnName = "Id", ColumnType = "int")]
+        public int EId
+            {
+                get { return _EId; }
+                set { _EId = value; }
+            }
+        [Column(ColumnName = "Name", ColumnType = "varchar(50)")]
+        public string Ename
+            {
+                get { return _EName; }
+                set { _EName = value; }
+            }
+        [Column(ColumnName = "Address", ColumnType = "varchar(50)")]
+        public string EAddress
+            {
+                get { return _EAddress; }
+                set { _EAddress = value; }
+            }
+
+        }
+    }
+
+
