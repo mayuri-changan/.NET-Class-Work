7b3be288e9067ff1893b643a1fa3085f83cf5769
mayuri-changan <mayuri2001changan@gmail.com>
2026-07-06 11:39:21 +0530

    Demo33

---
+++ Demo33/CompanyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+namespace Demo33
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CompanyInfoAttribute : Attribute
+    {
+        private string _CompanyName;
+        private string _DeveloperName;
+
+        public string DeveloperName
+        {
+            get { return _DeveloperName; }
+            set
+            {
+                _DeveloperName = value;
+            }
+        }
+            public string CompanyName
+        {
+            get { return _CompanyName; }
+            set
+            {
+                _CompanyName = value;
+            }
+
+    }
+
+    }
+}
