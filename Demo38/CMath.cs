51be8c25b8cfb8514236def2cebe4f9ec830b43b
mayuri-changan <mayuri2001changan@gmail.com>
2026-07-07 18:50:22 +0530

    Demo38

---
+++ Demo38/CMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo38
+{
+    public partial class CMath
+    {
+        public void Addd(int x, int y)
+        {
+            Console.WriteLine($"Add = {x + y}");
+        }
+        public void Subb(int x, int y)
+        {
+            Console.WriteLine($"sub={x-y}");
+        }
+
+    }
+}
