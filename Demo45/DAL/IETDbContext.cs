9185a457a94dd885b98c631c45507a4474273508
mayuri-changan <mayuri2001changan@gmail.com>
2026-07-09 22:59:49 +0530

    Demo45

---
+++ Demo45/DAL/IETDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection.Metadata;
+using Demo45.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.Extensions.Configuration;
+
+namespace Demo45.DAL
+{
+    public class IETDbContext : DbContext
+    //{
+    //    public DbSet<Emp> emps { get; set; }
+
+
+    //  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    {
+    //        var builder = new ConfigurationBuilder();
+
+    //        builder.SetBasePath(Directory.GetCurrentDirectory());
+
+    //        builder.AddJsonFile("appsettings.json");
+
+    //        IConfiguration config = builder.Build();
+
+    //        optionsBuilder.UseSqlServer(config.GetConnectionString("conStr"));
+    //    }
+    //}
+    {    
+            public DbSet<Emp> emps {  get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           var builder= new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings");
+           IConfiguration config= builder.Build();
+           optionsBuilder.UseSqlServer (config.GetConnectionString(""));
+        }
+
+
+    
+    
+    }
+}
+
---
+++ Demo45/Program.cs
@@ -0,0 +1,44 @@
+﻿using Demo45.DAL;
+using Demo45.Models;
+
+namespace Demo45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IETDbContext _dbContext = new IETDbContext();
+
+            #region Select Query
+            var result = _dbContext.emps.ToList();
+            foreach (Emp emp in result)
+            {
+                Console.WriteLine($"Id: {emp.Id},Name:{emp.Name},Address: {emp.Address}");
+            }
+            #endregion
+
+            #region Insert
+            //Emp empToBeInserted = new Emp() { Name = "Ron", Address = "Rome" };
+            //_dbContext.emps.Add(empToBeInserted);
+            //_dbContext.SaveChanges();
+            #endregion
+
+            #region Update
+            int id = 3;
+            Emp empToBeUpdated = _dbContext.emps.Find(id);
+            empToBeUpdated.Name = "Prachi";
+            empToBeUpdated.Address = "Nagar";
+
+            _dbContext.SaveChanges();
+            #endregion
+
+            #region Delete
+            //int idToBeDeleted = 2;
+            //Emp empToBDeleted = _dbContext.emps.Find(idToBeDeleted);
+            //_dbContext.emps.Remove(empToBDeleted);
+            //_dbContext.SaveChanges();
+            #endregion
+
+        }
+    }
+}
