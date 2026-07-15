a658cb978c7f4eebdbcc63cff151f2c5b72f8cc4
mayuri-changan <mayuri2001changan@gmail.com>
2026-07-16 01:12:25 +0530

    practice_1

---
+++ Practice_1/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practice_1.Models
+{
+    public class Category : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practice_1/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practice_1.Models
+{
+    public class Product : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
