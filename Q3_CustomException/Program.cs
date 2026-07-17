65f03856c8fe85cd6030991c52ab2eaf041408fe
mayuri-changan <mayuri2001changan@gmail.com>
2026-07-17 19:20:38 +0530

    Q2_CustomException

---
+++ Q3_CustomException/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Q3_CustomException
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter Age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                if (age < 18)
+                {
+                    throw new Exception("You are not eligible to vote.");
+                }
+
+                Console.WriteLine("You are eligible to vote.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
