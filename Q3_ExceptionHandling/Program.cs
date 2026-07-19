00bbaa4fd3c3474aae6c304531c12550060f6904
mayuri-changan <mayuri2001changan@gmail.com>
2026-07-19 23:09:38 +0530

    Q3_ExceptionHandling

---
+++ Q3_ExceptionHandling/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Q3_ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter First Number: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter Second Number: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                int result = a / b;
+
+                Console.WriteLine("Result = " + result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter only numbers.");
+            }
+            finally
+            {
+                Console.WriteLine("Program Finished.");
+            }
+        }
+    }
+}
